b93a0d856fa4d9aae516e59b0b4d42f2c576e391
Rob <rzc1@staff.staffs.ac.uk>
2025-10-08 16:30:54 +0100

    Push tests for stcolrm methods

--- tests/testthat/test-stcolrm.R
+++ tests/testthat/test-stcolrm.R
@@ -78,3 +78,37 @@ test_that("stcolrm full", {
 
 })
 
+test_that("stcolrm print", {
+
+  .data <- kmed::heart
+  .data$classN <- ifelse(.data$class == 0, 0, 1)
+
+  mfit <- stcolrm(classN ~ age + sex + trestbps + chol + restecg,
+                  data=.data)
+
+  .msg <- evaluate_promise(print(mfit))
+  expect_match(.msg$output, "Standardized Coefficients \\(simple-preSTD\\).*")
+  expect_equal(.msg$warnings, character())
+  expect_equal(.msg$messages, character())
+
+  .msg <- evaluate_promise(print(mfit, digits=10))
+  expect_match(.msg$output, "Standardized Coefficients \\(simple-preSTD\\).*")
+  expect_equal(.msg$warnings, character())
+  expect_equal(.msg$messages, character())
+
+})
+
+
+test_that("stcolrm summary", {
+
+  .data <- kmed::heart
+  .data$classN <- ifelse(.data$class == 0, 0, 1)
+
+  mfit <- stcolrm(classN ~ age + sex + trestbps + chol + restecg,
+                  data=.data)
+
+  .summary <- summary(mfit)
+
+  expect_s3_class(.summary, "tbl_df")
+  expect_equal(dim(.summary), c(6, 2))
+})
